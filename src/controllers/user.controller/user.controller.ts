5a53f23f4d4527e0bd26b247760d4ef6520b2f60
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-18 19:30:20 -0500

    create user controller

--- src/controllers/user.controller/user.controller.ts
+++ src/controllers/user.controller/user.controller.ts
@@ -1,16 +1,24 @@
 import { Request, Response, NextFunction } from "express"
-import { creatUser } from "../../services/dbQueries/user.services"
+import { creatUser } from "../../services/dbQueries/user.services.queries"
+import { createUserVal, userExistance } from "../../services/validations/user.validations/user.validations"
 
 /**
- * @description - SingUp Controller
+ * @description - SingUp Controller, create new user
  * @param req - The request object
  * @param res - The response object
  * @function creatUser - This function is used to create a new user
  * and resolve the response to the client
+ * @function createUserVal - This function is used to validate the user 
+ * @function userExistance - This function is used to check username and 
+ * email existence
  */
-const signUp =  (req:Request, res:Response) => {
+const signUp =  async (req:Request, res:Response) => {
+    let isValid:boolean = true; // isValid is true by default
 
-    creatUser()
+    isValid = createUserVal(req.body, res);
+    isValid && (isValid = await userExistance(req.body.email, req.body.username, res))
+    isValid && creatUser(req.body, res);
+
 }
 
 const signIn =  (req:Request, res:Response) => {
